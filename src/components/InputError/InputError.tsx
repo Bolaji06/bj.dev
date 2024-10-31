4f516a5120caff0d54b6eccf63739122a2fc55e4
Bolaji06 <joshspencer0603@gmail.com>
2024-10-31 23:28:32 +0100

    create form action for contact form and handle form input wit zod

---
+++ src/components/InputError/InputError.tsx
@@ -0,0 +1,17 @@
+
+interface InputErrorProps {
+    message: string;
+    path: string[];
+    name: string;
+}
+export default function InputError({
+  message,
+  path,
+  name,
+}: InputErrorProps): React.JSX.Element {
+  return (
+    <>
+      {path[0] === name && <p className="text-sm text-red-500">{message}</p>}
+    </>
+  );
+}
