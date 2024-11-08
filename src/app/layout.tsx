17c6921123f29fe8059c8213409eface707fd556
Bolaji06 <joshspencer0603@gmail.com>
2024-11-08 23:28:01 +0100

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { poppins } from "./fonts/font";
 import localFont from "next/font/local";
 import "./globals.css";
 import NavBar from "@/components/NavBar/NavBar";
-import Footer from "../components/Footer/Footer";
+// import Footer from "../components/Footer/Footer";
 
 
 const geistSans = localFont({
@@ -37,7 +37,7 @@ export default function RootLayout({
           <section className="max-w-4xl mx-auto">{children}</section>
           
         </main>
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
