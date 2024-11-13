dcf59d5b927a678ae8be4455dec2bbdad93b81b8
Bolaji06 <joshspencer0603@gmail.com>
2024-11-13 23:10:22 +0100

    add project page and project about page

---
+++ src/app/project/[title]/page.tsx
@@ -0,0 +1,39 @@
+import { getProject } from "@/data/fetchProject";
+
+import ProjectCard from "@/components/ProjectCard/ProjectCard";
+import { Project } from "@/definition/definition";
+
+export default async function ProjectPage({
+  params,
+}: {
+  params: { title: string };
+}): Promise<React.JSX.Element> {
+  const param = await params;
+  const title = param.title;
+  const decodeTitle = decodeURIComponent(title);
+
+  const response: Project = await getProject(decodeTitle);
+
+  const project = response.project;
+
+  return (
+    <>
+      <section className="py-14">
+        <header className="py-6">
+          <ProjectCard
+            title={project.title}
+            description={project.description}
+            stacks={project.stacks}
+            thumbnail={project.thumbnail}
+            url={project.url}
+            githubUrl={project.githubUrl}
+            gitMore="git"
+          />
+        </header>
+        <article>
+            {project.about}
+        </article>
+      </section>
+    </>
+  );
+}
--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,12 +1,41 @@
+import ProjectCard from "@/components/ProjectCard/ProjectCard";
+import { getProjects } from "@/data/fetchProject";
+import { IProjectResponse } from "@/definition/definition";
 
+export default async function ProjectPage() {
+  const response: IProjectResponse = await getProjects();
+  const projects = response.projects;
 
-export default function ProjectPage(): React.JSX.Element{
+  return (
+    <>
+      <section className="py-14 px-6">
+        <header className="py-10 space-y-5">
+          <h1 className="text-4xl md:text-5xl font-semibold">Projects</h1>
+          <p className="text-xl md:text-2xl font-medium text-primary-brand">
+            Curated list of projects have worked on
+          </p>
+        </header>
 
-    return(
-        <>
-            <section>
-                Project Page
-            </section>
-        </>
-    )
+        <div className="py-10">
+          <div className="grid lg:grid-cols-2 gap-8">
+            {projects.map((project) => {
+              return (
+                <ProjectCard
+                  key={project.id}
+                  title={project.title}
+                  description={project.description}
+                  stacks={project.stacks}
+                  thumbnail={project.thumbnail}
+                  url={project.url}
+                  githubUrl={project.githubUrl}
+                  gitMore="more"
+                  showFooter={false}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
