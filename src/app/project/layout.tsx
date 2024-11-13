811ae261cac6a0c58f1ff5b5a1c4a3b83f7d573d
Bolaji06 <joshspencer0603@gmail.com>
2024-11-13 23:13:47 +0100

    resolve footer module path

--- src/app/project/layout.tsx
+++ src/app/project/layout.tsx
@@ -1,5 +1,5 @@
 
-import Footer from "@/components/Footer/Footer";
+import Footer from "../../components/Footer/Footer";
 import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }){
