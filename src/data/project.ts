b84b605e1edeefd96536eabb0c4fa9c2952a845e
Bolaji06 <joshspencer0603@gmail.com>
2024-11-27 23:06:49 +0100

    make all Markdown text display properly

--- src/data/project.ts
+++ src/data/project.ts
@@ -14,6 +14,7 @@ import {
   SiHono,
   SiVercel,
   SiRender,
+  SiPostgresql,
 } from "react-icons/si";
 import { RiSecurePaymentFill } from "react-icons/ri";
 import { IconType } from "react-icons/lib";
@@ -34,4 +35,5 @@ export const stackIcons: Record<string, IconType> = {
   Render: SiRender,
   Vite: SiVite,
   Honojs: SiHono,
+  Postgres: SiPostgresql,
 };
