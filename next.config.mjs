27becb5c0d1a788b0c61107caaa54b4a3bc4807a
Bolaji06 <joshspencer0603@gmail.com>
2024-11-28 21:19:15 +0100

    add prose class to blog post article

--- next.config.mjs
+++ next.config.mjs
@@ -36,9 +36,3 @@ const withMDX = nextMDX({
 
 export default withMDX(nextConfig);
 
-// const withMDX = createMDX({
-//     // Add markdown plugins here, as desired
-//   })
-
-//   // Merge MDX config with Next.js config
-//   export default withMDX(nextConfig)
