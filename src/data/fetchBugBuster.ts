7c1b490bdde1ed9c325cecbbbe0a7e54ca27ab75
Bolaji06 <joshspencer0603@gmail.com>
2024-12-27 05:16:40 +0100

    solving bug

--- src/data/fetchBugBuster.ts
+++ src/data/fetchBugBuster.ts
@@ -1,17 +1,8 @@
 const API = `${process.env.NEXT_PUBLIC_BASE_API_ENDPOINT}`;
 
 export async function getBugBusterList() {
-  const options = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    next: {
-      tags: ["bug-busters"],
-    },
-  };
   try {
-    const response = await fetch(`${API}/bug`, options);
+    const response = await fetch(`${API}/bug`, { next: { tags: ["bug-busters"] } });
     if (!response.ok) {
       return "Error fetching bug buster";
     }
