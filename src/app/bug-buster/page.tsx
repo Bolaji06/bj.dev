363c6817a98418c9d0ba9574b98c3d5af42a16de
Bolaji06 <joshspencer0603@gmail.com>
2024-12-27 13:18:55 +0100

    solving deploy bug

--- src/app/bug-buster/page.tsx
+++ src/app/bug-buster/page.tsx
@@ -1,8 +1,10 @@
-import BugBusterList from "@/components/BugBusterList/BugBusterList";
+//import BugBusterList from "@/components/BugBusterList/BugBusterList";
 import { fetchBugBusterList } from "@/data/fetchBugBuster";
 import { IBugBusterListResponse } from "@/definition/definition";
+import dynamic from "next/dynamic";
 import { Suspense } from "react";
 
+const DynamicList = dynamic(() => import("@/components/BugBusterList/BugBusterList"));
 export default async function BugBusterPage() {
   const bugBusters: IBugBusterListResponse = await fetchBugBusterList();
 
@@ -19,7 +21,9 @@ export default async function BugBusterPage() {
           </div>
         </header>
         <Suspense fallback={<p>Loading...</p>}>
-          <BugBusterList bugBusterList={bugBusterList} />
+        { bugBusterList && 
+          <DynamicList bugBusterList={bugBusterList}/>
+        }
         </Suspense>
       </section>
     </>
