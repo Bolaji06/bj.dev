570ffcf6a32d882c0449c62f3658ec44711a9b75
Bolaji06 <joshspencer0603@gmail.com>
2024-12-29 20:15:45 +0100

    proper formatting of bug bugster content

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,16 @@
 import type { MDXComponents } from "mdx/types";
+import { ComponentPropsWithRef } from "react";
+import { highlight } from "sugar-high"
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
+  const myTags = {
+    code: ({ children, ...props}: ComponentPropsWithRef<'code'>) => {
+      const codeHTML = highlight(children as string);
+      return <code dangerouslySetInnerHTML={{__html: codeHTML}} {...props}/>
+    }
+  }
   return {
     ...components,
+    ...myTags,
   };
 }
