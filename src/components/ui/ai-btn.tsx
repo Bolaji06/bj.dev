217d45ffb12d476c36827c3df90ba9a1d8291155
Bolaji06 <joshspencer0603@gmail.com>
2025-01-28 21:57:44 +0100

    change the ai-chat routing to Link component

--- src/components/ui/ai-btn.tsx
+++ src/components/ui/ai-btn.tsx
@@ -1,7 +1,5 @@
-"use client";
-
 import { AiOutlineWechatWork } from "react-icons/ai";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 import {
   Tooltip,
@@ -11,23 +9,29 @@ import {
 } from "@/components/ui/tooltip";
 
 export default function AIChatButton() {
-  const router = useRouter();
   return (
     <>
-    
       <div
         data-testid="link"
         className="hidden fixed bottom-32 w-12 aspect-square right-6 lg:flex flex-col items-center justify-center font-medium text-sm gap-2 border-2 shadow-2xl border-white bg-gray-800 rounded-lg shadow-xl"
       >
         <TooltipProvider>
           <Tooltip>
-            <TooltipTrigger onClick={() => router.prefetch("/ai-chat")}>
-              <AiOutlineWechatWork size={25} className="font-semibold text-white" />
-            </TooltipTrigger>
+            <Link
+              href={"/ai-chat"}
+              prefetch={true}
+              className="flex justify-center items-center"
+            >
+              <TooltipTrigger>
+                <AiOutlineWechatWork
+                  size={25}
+                  className="font-semibold text-white"
+                />
+              </TooltipTrigger>
+            </Link>
             <TooltipContent className="mb-4 mr-2 max-w-32 bg-gray-700 text-white">
               <p className="text-xs text-center">
-                Chat with AI assistance to get all latest info about
-                Bolaji
+                Chat with AI assistance to get all latest info about Bolaji
               </p>
             </TooltipContent>
           </Tooltip>
